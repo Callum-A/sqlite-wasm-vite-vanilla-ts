b5569e025c53c7d25066344ee651b84740f942bc
Callum-A <callumanderson745@gmail.com>
2025-02-25 21:20:25 +0000

    Improv

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,22 @@ import connectToDB, {
   deleteDB,
   doesDBNameAlreadyExist,
   listAllDBs,
+  SqliteConnector,
 } from './db-wrapper';
+import { ulid } from 'ulid';
+
+const DB_NAME = 'MyDB2.db';
 
 const CREATE_PERSON_SQL = `
 CREATE TABLE person (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   age INTEGER NOT NULL
 )`;
 
 const CREATE_DOG_SQL = `
 CREATE TABLE dog (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   age INTEGER NOT NULL
 )`;
@@ -21,11 +25,54 @@ CREATE TABLE dog (
 const peopleList = document.getElementById('people');
 const dogsList = document.getElementById('dogs');
 
+function chunkArray<T>(array: T[], size: number) {
+  if (size <= 0) throw new Error('Size must be greater than zero');
+
+  let result = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+}
+
+const batchInsert = async (
+  db: SqliteConnector,
+  tableName: string,
+  rows: any[],
+  batchSize: number
+) => {
+  const chunks = chunkArray(rows, batchSize);
+  const keys = Object.keys(chunks[0][0]);
+  keys.sort();
+  console.log(keys);
+  const colStr = keys.join(', ');
+  const baseSql = `INSERT INTO ${tableName} (${colStr}) VALUES `;
+
+  for (const chunk of chunks) {
+    const params = [];
+    const sqlStrings = [];
+    for (const row of chunk) {
+      const sqlString = '(' + '?'.repeat(keys.length).split('').join(',') + ')';
+      sqlStrings.push(sqlString);
+      for (const k of keys) {
+        params.push(row[k]);
+      }
+    }
+
+    const sql = baseSql + sqlStrings.join(', ');
+    console.log('Executing', sql);
+    await db.queryMany(sql, params);
+    console.log('Done executing', sql);
+  }
+};
+
 class Person {
+  id: string;
   name: string;
   age: number;
 
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, id?: string) {
+    this.id = id ? id : ulid();
     this.name = name;
     this.age = age;
   }
@@ -35,15 +82,21 @@ class Person {
   }
 
   public static loadFromDB(row: any) {
-    return new Person(row['name'], row['age']);
+    return new Person(row['name'], row['age'], row['id']);
+  }
+
+  public static async batchInsert(db: SqliteConnector, people: Person[]) {
+    await batchInsert(db, 'person', people, 500);
   }
 }
 
 class Dog {
+  id: string;
   name: string;
   age: number;
 
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, id?: string) {
+    this.id = id ? id : ulid();
     this.name = name;
     this.age = age;
   }
@@ -53,17 +106,21 @@ class Dog {
   }
 
   public static loadFromDB(row: any) {
-    return new Dog(row['name'], row['age']);
+    return new Dog(row['name'], row['age'], row['id']);
+  }
+
+  public static async batchInsert(db: SqliteConnector, dogs: Dog[]) {
+    await batchInsert(db, 'dog', dogs, 500);
   }
 }
 
 const main = async () => {
   let needToCreateTables = true;
   console.log(await listAllDBs());
-  if (await doesDBNameAlreadyExist('test.db')) {
+  if (await doesDBNameAlreadyExist(DB_NAME)) {
     needToCreateTables = false;
   }
-  const myDb = await connectToDB('test.db');
+  const myDb = await connectToDB(DB_NAME);
 
   if (!myDb) {
     return;
@@ -74,24 +131,20 @@ const main = async () => {
     myDb.execute(CREATE_DOG_SQL);
   }
 
-  // Gets the ID
-  const row = await myDb.queryOne(
-    'INSERT INTO person (name, age) VALUES (?, ?) RETURNING id',
-    ['John', 24]
-  );
-  console.log('Got ID: ', row['id']);
-  await myDb.queryOne('INSERT INTO person (name, age) VALUES (?, ?)', [
-    'Sarah',
-    19,
-  ]);
-  await myDb.queryOne('INSERT INTO dog (name, age) VALUES (?, ?)', [
-    'Riley',
-    4,
-  ]);
-  await myDb.queryOne('INSERT INTO dog (name, age) VALUES (?, ?)', [
-    'Dexter',
-    3,
-  ]);
+  const peopleObjects: Person[] = [];
+  for (let i = 0; i < 500; i++) {
+    peopleObjects.push(new Person(i + 'Person', i));
+  }
+
+  await Person.batchInsert(myDb, peopleObjects);
+
+  const dogObjects: Dog[] = [];
+  for (let i = 0; i < 1000; i++) {
+    dogObjects.push(new Dog(i + 'Dog', i));
+  }
+
+  await Dog.batchInsert(myDb, dogObjects);
+
   const people = await myDb.queryMany(
     'SELECT * FROM person',
     [],
@@ -103,7 +156,11 @@ const main = async () => {
     li.textContent = JSON.stringify(person);
     peopleList?.appendChild(li);
   }
-  const dogs = await myDb.queryMany('SELECT * FROM dog', [], Dog.loadFromDB);
+  const dogs = await myDb.queryMany(
+    'SELECT * FROM dog WHERE id = ?',
+    [dogObjects[0].id],
+    Dog.loadFromDB
+  );
   for (const dog of dogs) {
     dog.woof();
     const li = document.createElement('li');
@@ -111,7 +168,7 @@ const main = async () => {
     dogsList?.appendChild(li);
   }
   await myDb.close();
-  await deleteDB('test.db');
+  await deleteDB(DB_NAME);
 };
 
 main();
